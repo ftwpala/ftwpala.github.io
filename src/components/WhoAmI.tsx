f01b2b49719f5cd1cad3be77586136fe1286c0b4
ftwpala <ftwpala@gmail.com>
2024-06-05 15:38:40 +0200

    sliding part of initial section, hardcoded height for now
    TODO: Make dynamic using react ref and offsetHeight

--- src/components/WhoAmI.tsx
+++ src/components/WhoAmI.tsx
@@ -1,63 +1,76 @@
 import { useEffect, useRef, useState } from "react";
+import "../components/WhoAmI.css";
 import MyPicture from "../images/dinkdonk.gif";
+import BackIcon from "../icons/MUI_Arrow_Back.svg";
 
 const WhoAmI = () => {
-  const [width, setWidth] = useState(0);
   const initialSection = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (initialSection.current != undefined) {
-      setWidth(initialSection.current.offsetWidth);
-      console.log(width);
-    }
-  }, [width]);
+  const slidingSection = useRef<HTMLDivElement>(null);
+  const sectionToggleButton = useRef<HTMLButtonElement>(null);
 
   return (
     <div ref={initialSection} className="section">
-      <div
-        style={{
-          display: "flex",
-          flexFlow: "row",
-        }}
-      >
-        <div style={{ marginRight: "50px" }}>
-          <img src={MyPicture}></img>
-        </div>
-        <div>
-          <div>Who am I?</div>
-          <div>
-            <ul>
-              <li>Item</li>
-              <li>Item</li>
-              <li>Item</li>
-              <li>Item</li>
-              <li>Item</li>
-            </ul>
+      <div style={{ width: "100%", display: "flex" }}>
+        <div
+          style={{
+            display: "flex",
+            flexFlow: "row",
+            margin: "auto",
+            minWidth: "500px",
+            width: "50%",
+          }}
+        >
+          <div style={{ marginRight: "50px" }}>
+            <img src={MyPicture}></img>
+          </div>
+          <div style={{ width: "100%" }}>
+            <div>Who am I?</div>
+            <div>
+              <ul>
+                <li>Item</li>
+                <li>Item</li>
+                <li>Item</li>
+                <li>Item</li>
+                <li>Item</li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div
-        className="sliding-part"
-        style={{
-          display: "flex",
-          flexFlow: "row",
-          left: width + 0 + "px",
-          position: "absolute",
-        }}
-      >
-        <div style={{ marginRight: "50px" }}>
-          <img src={MyPicture}></img>
-        </div>
         <div>
-          <div>Who am I?</div>
+          <button
+            ref={sectionToggleButton}
+            className="button"
+            style={{
+              position: "absolute",
+              right: "0px",
+              zIndex: "2",
+            }}
+            onClick={(event: React.MouseEvent<HTMLElement>) => {
+              if (!slidingSection.current!.classList.contains("show")) {
+                slidingSection.current!.classList.add("show");
+              } else {
+                slidingSection.current!.classList.remove("show");
+              }
+            }}
+          >
+            <img src={BackIcon}></img>
+          </button>
+        </div>
+      </div>
+      <div ref={slidingSection} className="sliding-part">
+        {/* <div style={{ marginRight: "50px" }}>
+          <img src={MyPicture}></img>
+        </div> */}
+        <div>
+          <div>My Qualifications!</div>
           <div>
             <ul>
-              <li>Item</li>
-              <li>Item</li>
-              <li>Item</li>
-              <li>Item</li>
-              <li>Item</li>
+              <li>I </li>
+              <li>Graduated</li>
+              <li>University</li>
+              <li>Bachelors</li>
+              <li>I</li>
+              <li>Promise</li>
             </ul>
           </div>
         </div>
